2fc8bc6ebd4c7e3f808c84c142c445393de9a268
denhur62 <63595768+denhur62@users.noreply.github.com>
2021-01-01 18:11:53 +0900

    카드2

---
+++ baekjoon/카드2.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+N = int(input())
+arr = deque([i for i in range(1, N+1)])
+
+while len(arr) != 1:
+    arr.popleft()
+    if len(arr) == 1:
+        break
+    arr.insert(len(arr)-1, arr.popleft())
+print(arr[0])
