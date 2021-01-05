29f14c3067a41f0fe62041e834461366f56c3d2c
denhur62 <63595768+denhur62@users.noreply.github.com>
2021-01-05 17:03:02 +0900

    튜플 리팩토링

--- programmers/level2/tuple.py
+++ programmers/level2/tuple.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def solution(s):
+    s = s[2:-2].split("},{")
+    s = list(map(lambda x: x.split(','), s))
+    s.sort(key=lambda x: len(x))
+    answer = []
+    for i in s:
+        for j in i:
+            if not int(j) in answer:
+                answer += [int(j)]
+    return answer
