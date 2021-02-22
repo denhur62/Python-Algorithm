6c40c93f3caca52fbd1d6ea9ea133d4880f39873
denhur62 <63595768+denhur62@users.noreply.github.com>
2021-02-23 02:35:27 +0900

    위장

--- programmers/level2/스토크.py
+++ programmers/level2/스토크.py
@@ -2,27 +2,43 @@ from collections import defaultdict
 from itertools import combinations
 
 
-def combi(n, m):
-    answer = 1
-    for i in range(n-m+1, n+1):
-        answer *= i
-    for i in range(1, m+1):
-        answer /= i
-    return answer
-
-
 def solution(clothes):
-    answer = len(clothes)
     dic = defaultdict(lambda: [])
     for i in clothes:
         dic[i[1]].append(i[0])
-    for i in range(2, len(dic.keys())):
-        c = combi
+    temp = 1
+    # dic = dict(map(len, dic))
+    for i in dic:
+        temp *= len(dic[i])+1
 
-    print(dic)
-    return answer
+    return temp-1
+
+    # def solution(clothes):
+    #     answer = len(clothes)
+    #     dic = defaultdict(lambda: [])
+    #     for i in clothes:
+    #         dic[i[1]].append(i[0])
+    #     for i in range(2, len(dic.keys())+1):
+    #         c = list(combinations(dic.keys(), i))
+    #         for i in c:
+    #             temp = 1
+    #             for j in i:
+    #                 temp *= len(dic[j])
+    #             answer += temp
 
 
-clothes = [["yellow_hat", "headgear"], [
-    "blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+    #     return answer
+clothes = [
+    ["a", "aa"],
+    ["b", "aa"],
+    ["c", "aa"],
+    ["a_a", "bb"],
+    ["b_b", "bb"],
+    ["c_c", "bb"],
+    ["aaa", "cc"],
+    ["bbb", "cc"],
+    ["ccc", "cc"]
+
+]
+
 print(solution(clothes))
