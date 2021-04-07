41c832f6e62c4256beee6679cc9d2d48f69f6cfa
denhur62 <63595768+denhur62@users.noreply.github.com>
2021-04-07 23:03:43 +0900

    ds

--- baekjoon/dfs,bfs/뱀과 사다리게임.py
+++ baekjoon/dfs,bfs/뱀과 사다리게임.py
@@ -1,49 +1,40 @@
 from collections import deque
 import sys
-N,M=map(int,sys.stdin.readline().split())
-ladder=[]
-for i in range(N):
-    NN,MM=map(int,input().split())
-    ladder.append((NN,MM))
-snake=[]
-for j in range(M):
-    NN,MM=map(int,input().split())
-    snake.append((NN,MM))
-def check(queue,s,c,visited):
-    for i1,i2 in ladder:
-        if s==i1 and visited[i2]==False:
-            visited[i2]=True
-            queue.append((i2,c))
-            return False
-    for j1,j2 in snake:
-        if s==j1 and visited[j2]==False:
-            visited[j2]=True
-            queue.append((j2,c))
-            return False
-    return True
+N, M = map(int, sys.stdin.readline().split())
+ladder = []
+for i in range(N+M):
+    NN, MM = map(int, input().split())
+    ladder.append((NN, MM))
+
+
+def check(a, b):
+    cnt = 0
+    for i in b:
+        if i[0] == a:
+            return cnt
+        cnt += 1
+    return False
+
+
 def dfs(start):
-    queue=deque()
-    answer=float('inf')
-    queue.append((start,0))
-    visited=[False]*101
+    queue = deque()
+    answer = float('inf')
+    queue.append((start, 0))
+    visited = [False]*101
     while queue:
-        s,c= queue.popleft()
-        if s==100:
-            if answer>c:
-                answer=c
-        elif s>100:
+        s, c = queue.popleft()
+        if s == 100:
+            return c
+        elif s > 100:
             continue
         else:
-            if check(queue,s,c,visited):
-                for i in range(1,7):
-                    if s+i<=100 and visited[s+i]==False:
-                        visited[s+i]=True
-                        queue.append((s+i,c+1))
-    return answer
-        
-        
-        
-
+            for i in range(1, 7):
+                if s+i <= 100:
+                    c = check(s+i, ladder)
+                    if c:
+                        queue.append((ladder[c][1], c))
+                    else:
+                        queue.append((s+i, c+1))
 
 
 print(dfs(1))
