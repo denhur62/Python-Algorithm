d49c809fbabf3702dcb7870c9a5e464c431aaf15
denhur62 <denhur62@naver.com>
2021-09-21 17:24:58 +0900

    2021-09-21

--- programmers/level2/큰 수 만들기.py
+++ programmers/level2/큰 수 만들기.py
@@ -2,16 +2,17 @@
 def solution(number, k):
     answer=[]
     for i in number:
+        print(answer,k)
         if not answer or answer[-1] > i or k==0:
             answer.append(i)
         else:
-            while k>=0 and answer and answer[-1] < i:
+            while k>0 and answer and answer[-1] < i:
                 answer.pop()
                 k-=1
             answer.append(i)
     if k!=0:
-        return "".join(answer[:k])
+        return "".join(answer[:len(number)-k])
     else:
         return "".join(answer)
 
-print(solution("1231234",3))
+print(solution("4177252841",4))
