8dc44d857e033fef2712c28904e828d3c74ee142
denhur62 <denhur62@naver.com>
2021-09-25 19:00:51 +0900

    2021-090-25

--- programmers/level3/보석 쇼핑.py
+++ programmers/level3/보석 쇼핑.py
@@ -1,18 +1,27 @@
-
-
+from collections import defaultdict
 def solution(gems):
-    N = len(set(gems))
-    last = len(gems)
+    lsg=len(set(gems))
+    dic=defaultdict(int)
     start, end = 0, 0
-    while end < last:
-        if len(set(gems[start:end])) == N:
-            break
-        end += 1
-    while start < end and len(set(gems[start:end])) == N:
-        start += 1
+    short=len(gems)
+    answer=[]
+    while end<len(gems):
+        dic[gems[end]]+=1
+        if len(dic)== lsg:
+            while start<=end and len(dic)==lsg:
+                print(dic,start,end)
+                if dic[gems[start]]>1:
+                    dic[gems[start]]-=1
+                    start+=1
+                else:
+                    del dic[gems[start]]
+                    start+=1
+                if short > end -start:
+                    short=end-start
+                    answer=[start,end+1]
+        end+=1
+    return answer
 
-    return [start, end]
 
-
-gems = ["AA", "AB", "AC", "AA", "AC"]
+gems=["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
 print(solution(gems))
